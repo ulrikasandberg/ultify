12016eb3fb6d7368f7d240dd442c5191c6f764d3
ulrikasandberg <ulrika@warpmail.net>
2016-10-11 16:14:58 +0200

    It is possible to search on track, artist, playlist and album.

--- app/search/ultify_search.controller.js
+++ app/search/ultify_search.controller.js
@@ -25,48 +25,126 @@ SOFTWARE.
 angular.
 module('UltifySearch').
 controller('UltifySearchController', function ($scope, $http, UrlManager) {
-	$scope.artist = '';
+	$scope.query = '';
+	$scope.type = 'track';
 	
 	var manageResponse = function(response) {
 		var resArray = [];
-		var items = (response.data.artists) ? response.data.artists.items : null;
-		var i, j, tmp, imgLen;
-		$scope.total = response.data.artists.total;
-		$scope.next = response.data.artists.next;
-		$scope.previous = response.data.artists.previous;
-		$scope.limit = response.data.artists.limit;
-		$scope.offset = response.data.artists.offset;
+		var i, j, tmp, imgLen, items;
+		
+		switch($scope.type) {
+			case 'track':
+				items = (response.data.tracks) ? response.data.tracks.items : null;
+				$scope.total = response.data.tracks.total;
+				$scope.next = response.data.tracks.next;
+				$scope.previous = response.data.tracks.previous;
+				$scope.limit = response.data.tracks.limit;
+				$scope.offset = response.data.tracks.offset;
+				$scope.colNameArr = ['Namn','Album','Artist(er)','Spela'];
 	
-		if(items) {
-			for(i = 0; i < items.length; i++) {
-				tmp = {};
-				imgLen = items[i].images.length;
-				tmp.img = (imgLen > 0) ? items[i].images[imgLen - 1].url : '';
-				tmp.name = items[i].name;
-				tmp.followers = items[i].followers.total;
-				tmp.genres = items[i].genres.join(', ');
-				tmp.play = items[i].external_urls.spotify;
-				resArray.push(tmp);
-			}
+				if(items) {
+					for(i = 0; i < items.length; i++) {
+						tmp = {};
+						imgLen = items[i].album.images.length;
+						tmp.img = (imgLen > 0) ? items[i].album.images[imgLen - 1].url : '';
+						tmp.name = items[i].name;
+						tmp.col3 = items[i].album.name;
+						tmp.col4 = items[i].artists.map(function(a) { return a.name }).join(', ');
+						tmp.play = items[i].external_urls.spotify;
+						resArray.push(tmp);
+					}
+				}
+				$scope.res = resArray;
+				break;
+			case 'artist':
+				items = (response.data.artists) ? response.data.artists.items : null;
+				$scope.total = response.data.artists.total;
+				$scope.next = response.data.artists.next;
+				$scope.previous = response.data.artists.previous;
+				$scope.limit = response.data.artists.limit;
+				$scope.offset = response.data.artists.offset;
+				$scope.colNameArr = ['Namn','Följare','Genres','Spela'];
+	
+				if(items) {
+					for(i = 0; i < items.length; i++) {
+						tmp = {};
+						imgLen = items[i].images.length;
+						tmp.img = (imgLen > 0) ? items[i].images[imgLen - 1].url : '';
+						tmp.name = items[i].name;
+						tmp.col3 = items[i].followers.total;
+						tmp.col4 = items[i].genres.join(', ');
+						tmp.play = items[i].external_urls.spotify;
+						resArray.push(tmp);
+					}
+				}
+				$scope.res = resArray;
+				break;
+			case 'album':
+				items = (response.data.albums) ? response.data.albums.items : null;
+				$scope.total = response.data.albums.total;
+				$scope.next = response.data.albums.next;
+				$scope.previous = response.data.albums.previous;
+				$scope.limit = response.data.albums.limit;
+				$scope.offset = response.data.albums.offset;
+				$scope.colNameArr = ['Namn','Typ','','Spela'];
+	
+				if(items) {
+					for(i = 0; i < items.length; i++) {
+						tmp = {};
+						imgLen = items[i].images.length;
+						tmp.img = (imgLen > 0) ? items[i].images[imgLen - 1].url : '';
+						tmp.name = items[i].name;
+						tmp.col3 = items[i].album_type;
+						tmp.col4 = '';
+						tmp.play = items[i].external_urls.spotify;
+						resArray.push(tmp);
+					}
+				}
+				$scope.res = resArray;
+				break;
+			case 'playlist':
+				items = (response.data.playlists) ? response.data.playlists.items : null;
+				$scope.total = response.data.playlists.total;
+				$scope.next = response.data.playlists.next;
+				$scope.previous = response.data.playlists.previous;
+				$scope.limit = response.data.playlists.limit;
+				$scope.offset = response.data.playlists.offset;
+				$scope.colNameArr = ['Namn','Ägare','Antal spår','Spela'];
+	
+				if(items) {
+					for(i = 0; i < items.length; i++) {
+						tmp = {};
+						imgLen = items[i].images.length;
+						tmp.img = (imgLen > 0) ? items[i].images[imgLen - 1].url : '';
+						tmp.name = items[i].name;
+						tmp.col3 = items[i].owner.id;
+						tmp.col4 = items[i].tracks.total;
+						tmp.play = items[i].external_urls.spotify;
+						resArray.push(tmp);
+					}
+				}
+				$scope.res = resArray;
+				break;
+			default:
+				break;
 		}
-		$scope.res = resArray;
 	};
 	
 	$scope.search = function() {
-		if($scope.artist) {
-			$http.get(UrlManager.searchEndpoint($scope.artist, 'artist')).then(manageResponse);
+		if($scope.query) {
+			$http.get(UrlManager.searchEndpoint($scope.query, $scope.type)).then(manageResponse);
 		}
 	};
 	
 	$scope.stepNext = function() {
 		if($scope.next) {
-			$http.get($scope.next, 'artist').then(manageResponse);
+			$http.get($scope.next).then(manageResponse);
 		}
 	};
 	
 	$scope.stepPrev = function() {
 		if($scope.previous) {
-			$http.get($scope.previous, 'artist').then(manageResponse);
+			$http.get($scope.previous).then(manageResponse);
 		}
 	};
 });
